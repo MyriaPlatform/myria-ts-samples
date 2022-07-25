752472d5b9ff3350503c3ab16d036181bd01f739
dmitrydao <dmitrydaopro@gmail.com>
2022-07-25 13:43:13 +0700

    add project detail

--- src/projects/project-detail.ts
+++ src/projects/project-detail.ts
@@ -1,22 +1,19 @@
 import { Modules } from "myria-core-sdk";
 import { ProjectResponse } from "myria-core-sdk/dist/types/src/types/ProjectTypes";
 
-const id: string = "10";
+const id: string = "5";
 
-export async function getProjectDetail() {
+(async (): Promise<void> => {
   const projectModule: Modules.ProjectModule = new Modules.ProjectModule();
 
-  let projectDetailResult: ProjectResponse | undefined;
+  let projectDetailResponse: ProjectResponse | undefined;
   try {
     console.log(`Retrieving details of the project #${id}...`);
-    projectDetailResult = await projectModule.getProjectDetail(id);
+    projectDetailResponse = await projectModule.getProjectDetail(id);
   } catch (error) {
     throw new Error(JSON.stringify(error, null, 2));
   }
 
-  console.log("Requested project result:");
-  console.log(JSON.stringify(projectDetailResult, null, 2));
-  return projectDetailResult;
-}
-
-getProjectDetail();
+  console.log("Requested project response:");
+  console.log(JSON.stringify(projectDetailResponse, null, 2));
+})();
