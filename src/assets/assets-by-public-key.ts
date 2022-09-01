b7d9d8395ea559e6548f5232d9b2d6374c8a77d4
markofdao <dmitrydaopro@gmail.com>
2022-09-01 16:02:45 +0700

    add assets samples

---
+++ src/assets/assets-by-public-key.ts
@@ -0,0 +1,30 @@
+import { IMyriaClient, ModuleFactory, MyriaClient, OnchainAssetManager } from "myria-core-sdk";
+import config from "../config";
+
+(async (): Promise<void> => {
+  const publicKey = config.public_key;
+  const env = config.environment;
+
+  const iClient: IMyriaClient = {
+    provider: null,
+    networkId: null,
+    web3: null,
+    env: env
+  };
+  const mClient = new MyriaClient(iClient);
+  const moduleFactory = new ModuleFactory(mClient);
+  const assetManager = moduleFactory.getAssetOnchainManager();
+
+  let assetsResponse;
+  try {
+    console.log(`Retrieving assets...`);
+    assetsResponse = await assetManager.getAssetsByOwnerPublicKey(
+      publicKey
+    );
+  } catch (error) {
+    throw new Error(JSON.stringify(error, null, 2));
+  }
+
+  console.log("Requested assets response:");
+  console.log(JSON.stringify(assetsResponse, null, 2));
+})();
