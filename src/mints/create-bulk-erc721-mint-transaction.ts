cc94436bca32ad63a92a90bcc35fe6e73aeb63a3
markofdao <dmitrydaopro@gmail.com>
2022-09-28 13:43:43 +0700

    update bulk minting script

--- src/mints/create-bulk-erc721-mint-transaction.ts
+++ src/mints/create-bulk-erc721-mint-transaction.ts
@@ -1,58 +1,55 @@
 import {
   MintingManager,
-  FeeType,
-  BulkMintableERC721Params,
-  MintAssetErc721Info
+  MintERC721Params,
+  FeeType
 } from "myria-core-sdk";
 import config from "../config";
 
 (async (): Promise<void> => {
+  const timer = (ms: any) => new Promise(res => setTimeout(res, ms));
+
   const env = config.environment;
   const mintingManager: MintingManager = new MintingManager(env);
-
+  
+  const feePercentage = 2;
   const startTokenId = 1;
   const endTokenId = 10;
-  const feePercentage = 2;
 
   const starkKey: string = config.stark_key;
   const contractAddress: string = config.collection_contract_address;
   const metadataApiUrl: string = config.metadata_api_url;
   const royaltyRecipient: string = config.public_key;
 
-  const assetsToMint: MintAssetErc721Info[] = [];
-
-  for (let i = startTokenId; i <= endTokenId; i++) {
-    const asset: MintAssetErc721Info = {
-      uri: `${metadataApiUrl}/${i}`,
-      tokenId: String(i),
-      description: "NFT",
-      fees: [{
-        percentage: feePercentage,
-        receiptAddress: royaltyRecipient,
-        feeType: FeeType.ROYALTY
-      }]
-    };
-    assetsToMint.push(asset);
-  }
-
-  const mintERC721Params: BulkMintableERC721Params = {
-    starkKey: starkKey,
-    contractAddress: contractAddress,
-    assets: assetsToMint,
-    isSupportGetBulkMetadata: true,
-    fees: [{
-      percentage: feePercentage,
-      receiptAddress: royaltyRecipient,
-      feeType: FeeType.ROYALTY
-    }]
-  };
-
+  let bulkMintResult = [];
   try {
     console.log("Initiating a bulk minting...");
-    const mintResult = await mintingManager.bulkMintableNftERC721(mintERC721Params);
-    console.log("Bulk minting is completed.");
-    console.log(mintResult);
+    for (let i = startTokenId; i < endTokenId; i++) {
+      const params: MintERC721Params = {
+        starkKey: starkKey,
+        contractAddress: contractAddress,
+        uri: `${metadataApiUrl}/${i}`,
+        tokenId: String(i),
+        description: "mry asset",
+        fees: [
+          {
+            percentage: feePercentage,
+            receiptAddress: royaltyRecipient,
+            feeType: FeeType.ROYALTY
+          },
+        ],
+      };
+      const mintTransactionResponse = await mintingManager.createMintTransactionERC721(
+        params
+      );
+      if(mintTransactionResponse) {
+        console.log(`Minted asset #${i}`);
+        bulkMintResult.push(mintTransactionResponse);
+      }
+      await timer(2000);
+    }
+    console.log(`Bulk mint is completed. Minted ${bulkMintResult.length} assets...`);
   } catch (error) {
-    console.log(error);
+    throw new Error(JSON.stringify(error, null, 2));
   }
+  // console.log(bulkMintResult);
 })();
