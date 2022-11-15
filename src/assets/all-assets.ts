72a7395a799bd166aeab49b7a8017b87dce461f5
markofdao <dmitrydaopro@gmail.com>
2022-11-15 13:51:40 +0700

    add all-assets

---
+++ src/assets/all-assets.ts
@@ -0,0 +1,27 @@
+import { IMyriaClient, ModuleFactory, MyriaClient } from "myria-core-sdk";
+import config from "../config";
+
+(async (): Promise<void> => {
+  const env = config.environment;
+  
+  const iClient: IMyriaClient = {
+    provider: null,
+    networkId: null,
+    web3: null,
+    env: env
+  };
+  const mClient = new MyriaClient(iClient);
+  const moduleFactory = new ModuleFactory(mClient);
+  const assetManager = moduleFactory.getAssetOnchainManager();
+
+  let assetsResponse;
+  try {
+    console.log(`Retrieving assets...`);
+    assetsResponse = await assetManager.getAssets();
+  } catch (error) {
+    throw new Error(JSON.stringify(error, null, 2));
+  }
+
+  console.log("Response:");
+  console.log(JSON.stringify(assetsResponse, null, 2));
+})();
