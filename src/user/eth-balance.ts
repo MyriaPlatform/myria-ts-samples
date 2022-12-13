414d97220f0b79cf931245dab9e03cb77a1ea938
markofdao <dmitrydaopro@gmail.com>
2022-12-13 16:17:09 +0700

    query user balances

---
+++ src/user/eth-balance.ts
@@ -0,0 +1,23 @@
+import { IMyriaClient, ModuleFactory, MyriaClient } from "myria-core-sdk";
+import config from "../config";
+
+(async (): Promise<void> => {
+  const env = config.environment;
+  const starkKey = config.stark_key;
+
+  const iClient: IMyriaClient = {
+    provider: null,
+    networkId: null,
+    web3: null,
+    env: env
+  };
+  const mClient = new MyriaClient(iClient);
+  const moduleFactory = new ModuleFactory(mClient);
+  const userManager = moduleFactory.getUserManager();
+
+  console.log(`Retrieving user balance...`);
+  const response = await userManager.getBalanceETH(starkKey);
+
+  console.log("Response:");
+  console.log(JSON.stringify(response, null, 2));
+})();
